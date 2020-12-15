4d1ca2549692bfde9c5e1d5e2ffaf695f262be5e
dextroza11 <nazaruk_98@bk.ru>
2020-12-15 23:06:45 +0300

    класс точка

--- tetris/Program.cs
+++ tetris/Program.cs
@@ -8,21 +8,13 @@ namespace tetris
 {
     class Program
     {
-        static void Drow(int x, int y, char c)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(c);
-            
-        }
+        
         static void Main(string[] args)
         {
             Console.SetWindowSize(60, 40);
             Console.SetBufferSize(60, 40);
-            int x1 = 2;
-            int y1 = 3;
-            char c = '*';
-            Drow(x1, y1, c);
-            Drow(3, 4, c);
+            Point p1 = new Point(2,3, '*');
+            p1.Draw();
             
         }
     }
